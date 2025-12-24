a70b9a4d6a5fbf57d1077fe7058c6ef7504bf859
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-24 13:42:37 +0200

    Refactor HomePage interactions for stability

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,27 +1,23 @@
 package pages;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     protected WebDriver driver;
-
+    protected WebDriverWait wait;
+    public  SignupModel signupModel;
+    public LoginModal loginModal;
     public HomePage(WebDriver driver){
         this.driver = driver;
-    }
-    //1. SignUp
-    private By signupLink = By.id("signin2");
-    private By signupUsername = By.id("sign-username");
-    private By signupPassword = By.id("sign-password");
-    private By signupButton = By.cssSelector("button[onclick='register()']");
-    public String clickSignup(String username, String password){
-    driver.findElement(signupLink).click();
-    driver.findElement(signupUsername).sendKeys(username);
-    driver.findElement(signupPassword).sendKeys(password);
-    driver.findElement(signupButton).click();
-    String alertStatus  = driver.switchTo().alert().getText();
-    driver.switchTo().alert().accept();
-    return alertStatus;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        signupModel = new SignupModel(driver);
+        loginModal = new LoginModal(driver);
     }
 
 
