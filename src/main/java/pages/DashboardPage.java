2f6cbfd9bb2af92f3adebd6e26d98f89fbbea545
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-24 13:42:30 +0200

    Fix stale element handling in DashboardPage

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -1,11 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DashboardPage {
     protected  WebDriver driver ;
+    protected WebDriverWait wait;
     DashboardPage (WebDriver driver){
         this.driver = driver ;
+        this.wait =new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+    private By nameofuser = By.id("nameofuser");
+    public String getStatus (){
+        return  wait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(nameofuser)).getText();
+
     }
 
 }
