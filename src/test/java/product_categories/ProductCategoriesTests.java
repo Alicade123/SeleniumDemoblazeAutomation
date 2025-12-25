0ec143ff8322ec930ebdd34cca849efbe22f4850
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-26 01:25:47 +0200

    The new testing fiel added to test the product category browsing the user provide corresponding category and the page filter the correct products in selected category and show them on the page

--- src/test/java/product_categories/ProductCategoriesTests.java
+++ src/test/java/product_categories/ProductCategoriesTests.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+package product_categories;
 
-public class product_categories {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+public class ProductCategoriesTests extends BaseTests {
+@Test
+    public void  testProductCategory() throws InterruptedException {
+    String category1 = "Phone";
+    String category2 = "Laptop";
+    String category3 = "Monitor";
+    homePage.productCategory.clickProductCategory(category3.toUpperCase());
+}
 }
