1aecaed059929262a47ea68c8c0c60dc70f67b2a
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-25 21:40:51 +0200

    The new changes added into homepage to allow the communication of our pom design between pages and tests of navigation bar

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,9 +1,6 @@
 package pages;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -13,11 +10,13 @@ public class HomePage {
     protected WebDriverWait wait;
     public  SignupModel signupModel;
     public LoginModal loginModal;
+    public Navigation navigation;
     public HomePage(WebDriver driver){
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         signupModel = new SignupModel(driver);
         loginModal = new LoginModal(driver);
+        navigation = new Navigation(driver);
     }
 
 
