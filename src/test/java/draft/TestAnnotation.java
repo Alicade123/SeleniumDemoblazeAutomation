45f74e28f704147056478264aaa1896194ef5108
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-29 01:05:06 +0200

    The changes added in the test annotation test to run test across the multiple browsers

--- src/test/java/draft/TestAnnotation.java
+++ src/test/java/draft/TestAnnotation.java
@@ -6,24 +6,40 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestAnnotation {
-    private WebDriver driver;
+WebDriver driver;
     @Test
-    @Parameters ({"URL", "BrowserType"})
-    public void runSite(String url , String browserType){
-         if(browserType.equalsIgnoreCase("internet explorer")){
-             driver = new InternetExplorerDriver();
-         }else if(browserType.equalsIgnoreCase("chrome")){
-             driver = new ChromeDriver();
-         }else if (browserType.equalsIgnoreCase("firefox")){
-             driver = new FirefoxDriver();
-         }
-         driver.get(url);
-         driver.manage().window().maximize();
-        System.out.println("The browser type is: "+browserType);
-        System.out.println("The site title: "+driver.getTitle());
-        System.out.println("The closed browser is: "+browserType);
-        driver.quit();
+    @Parameters({"URL", "BrowserType"})
+    public void runSite(String url, String browserType){
+        switch(browserType.toLowerCase()) {
+            case "chrome": {
+//                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            }
+            case "firefox": {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            }
+            case "internet explorer": {
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            }
+            default: throw new IllegalArgumentException("Unsupported browser: " + browserType);
+        }
+
+        try {
+            driver.get(url);
+            driver.manage().window().maximize();
+            System.out.println("The browser type is: " + browserType);
+            System.out.println("The site title: " + driver.getTitle());
+        } finally {
+            driver.quit();
+        }
     }
+
 }
