99f26c55fb5ad0ba0542f839a119e117548a5817
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-03 23:28:36 +0200

    The new change I added in baseTests to takescreenshot after each method

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,11 +1,20 @@
 package base;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 import pages.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BaseTests {
     protected WebDriver driver;
@@ -18,9 +27,21 @@ public class BaseTests {
         homePage = new HomePage(driver);
         driver.manage().window().maximize();
     }
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) throws IOException{
+        TakesScreenshot camera = ((TakesScreenshot)driver);
+        File img = camera.getScreenshotAs(OutputType.FILE);
+        LocalDateTime now =LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd dd-HH-mm");
+        String formattedTime = now.format(formatter);
+        if(result.getStatus()==ITestResult.FAILURE)Files.move(img.toPath(), new File("resources/failed/img"+formattedTime+".png").toPath());
+        else if(result.getStatus()==ITestResult.SUCCESS)Files.move(img.toPath(), new File("resources/passed/img"+formattedTime+".png").toPath());
+        else System.out.println("Unexpected Error occurred");
+    }
 
-//    @AfterClass
+    @AfterClass
     public void tearDown(){
         if(driver!=null) driver.quit();
     }
+
 }
